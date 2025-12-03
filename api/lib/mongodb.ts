e77e25e3f1fe2b68399c29f90ef8ea63d57f6d31
NayMcE <naomi.mcewan2@admiralgroup.co.uk>
2025-12-03 19:39:44 +0000

    Add tlsInsecure option to MongoDB connection

--- api/lib/mongodb.ts
+++ api/lib/mongodb.ts
@@ -8,18 +8,24 @@ if (!process.env.MONGODB_URI) {
   throw new Error('Please add your MongoDB URI to .env.local');
 }
 
+const mongoOptions = {
+  retryWrites: true,
+  w: 'majority' as const,
+  tlsInsecure: true,
+};
+
 if (process.env.NODE_ENV === 'development') {
   let globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>;
   };
 
   if (!globalWithMongo._mongoClientPromise) {
-    const client = new MongoClient(uri);
+    const client = new MongoClient(uri, mongoOptions);
     globalWithMongo._mongoClientPromise = client.connect();
   }
   clientPromise = globalWithMongo._mongoClientPromise;
 } else {
-  const client = new MongoClient(uri);
+  const client = new MongoClient(uri, mongoOptions);
   clientPromise = client.connect();
 }
 
